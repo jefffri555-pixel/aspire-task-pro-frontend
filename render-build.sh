a3648f8061accd534911877a175d64f63c80e70e
Vibhu Travel Hub <vibhutravelhub66@gmail.com>
2026-08-13 10:43:33 +0530

    Fix Render Flutter SDK path

--- render-build.sh
+++ render-build.sh
@@ -1,14 +1,21 @@
 #!/usr/bin/env bash
 set -e
 
-if [ ! -d "$HOME/flutter" ]; then
-  git clone https://github.com/flutter/flutter.git -b stable --depth 1 "$HOME/flutter"
+FLUTTER_DIR="$HOME/flutter"
+
+if [ ! -d "$FLUTTER_DIR/bin" ]; then
+  rm -rf "$FLUTTER_DIR"
+  git clone https://github.com/flutter/flutter.git \
+    --branch stable \
+    --depth 1 \
+    "$FLUTTER_DIR"
 fi
 
-export PATH="$HOME/flutter/bin:$PATH"
+export PATH="$FLUTTER_DIR/bin:$PATH"
 
-flutter config --enable-web
-flutter pub get
-flutter build web --release
+"$FLUTTER_DIR/bin/flutter" --version
+"$FLUTTER_DIR/bin/flutter" config --enable-web
+"$FLUTTER_DIR/bin/flutter" pub get
+"$FLUTTER_DIR/bin/flutter" build web --release
 
 echo "Flutter web build completed successfully."
